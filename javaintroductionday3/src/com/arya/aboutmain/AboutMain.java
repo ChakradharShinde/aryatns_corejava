784e1f04bba5affc4b45f3d0160f4e5eed7f234f
shindechakradhar1 <shindechakradhar1@gmail.com>
2024-01-20 11:00:48 +0530

    Commit version 1

---
+++ javaintroductionday3/src/com/arya/aboutmain/AboutMain.java
@@ -0,0 +1,22 @@
+package com.arya.aboutmain;
+
+public class AboutMain {
+
+//	public static void main(String[] args) {
+//		System.out.println("Hi");
+//	}
+	
+	// main method not found
+//	private static void main(String[] args) {
+//		System.out.println("hi");
+//	}
+	
+	
+	// main method is not static
+//	private void main(String[] args) {
+//		System.out.println("hi");
+//	}
+	
+	
+	
+}
---
+++ javaintroductionday3/src/com/arya/identifiers/Identifiers.java
@@ -0,0 +1,17 @@
+package com.arya.identifiers;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+//		// Syntaxt error on token @
+//		int @number1=10;
+		//// Syntaxt error on token 213
+//		int 213Number2=20;
+		// Syntaxt error on token 3
+//		int number 3=10;
+
+		int number4=10;
+		System.out.println(number4);
+	}
+
+}
